ebe760e08490a06f40c29a10b96932cabcca0bf6
Haroon Yousaf <haroonubl0177@hotmail.com>
2023-08-30 18:58:55 +0500

    change

--- config/permissions.php
+++ config/permissions.php
@@ -119,6 +119,103 @@ return [
       ],
     
     ],
-  ]
+  ],
+  
+  
+  
+  /*----------------------------------------
+ *   Slides Permissions
+ * ----------------------------------------
+ */
+  'Slides' => [
+  'title'       => 'Slides',
+  'description' => 'Manage Slides Permissions',
+  'abilities'   => [
+    
+    'manage slides' => [
+      'title' => 'Manage Slide Listing',
+    ],
+    
+    'add slide' => [
+      'title' => 'Add Slide',
+    ],
+    
+    'view slide' => [
+      'title' => 'View Slide',
+    ],
+    
+    'edit slide' => [
+      'title' => 'Edit Slide',
+    ],
+    
+    'delete slide' => [
+      'title' => 'Delete Slide',
+    ],
+  
+  ],
+],
+  /*----------------------------------------
+*   Gallery Permissions
+* ----------------------------------------
+*/
+  'Gallery' => [
+    'title'       => 'Gallery',
+    'description' => 'Manage Gallery Permissions',
+    'abilities'   => [
+    
+      'manage gallery' => [
+        'title' => 'Manage Gallery Listing',
+      ],
+    
+      'add gallery' => [
+        'title' => 'Add Gallery',
+      ],
+    
+      'view gallery' => [
+        'title' => 'View Gallery',
+      ],
+    
+      'edit gallery' => [
+        'title' => 'Edit Gallery',
+      ],
+    
+      'delete gallery' => [
+        'title' => 'Delete Gallery',
+      ],
+  
+    ],
+],
+
+  /*----------------------------------------
+*   Gallery Permissions
+* ----------------------------------------
+*/
+  'News & Events' => [
+    'title'       => 'News & Events',
+    'description' => 'Manage News & Events Permissions',
+    'abilities'   => [
+    
+      'manage events' => [
+        'title' => 'Manage News & Events Listing',
+      ],
+    
+      'add events' => [
+        'title' => 'Add News & Events',
+      ],
+    
+      'view events' => [
+        'title' => 'View News & Events',
+      ],
+    
+      'edit events' => [
+        'title' => 'Edit News & Events',
+      ],
+    
+      'delete events' => [
+        'title' => 'Delete News & Events',
+      ],
+  
+    ],
+  ],
 
 ];
