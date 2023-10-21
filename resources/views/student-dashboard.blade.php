078119bb0dc3f80057f31dd362347c681b660a7d
Haroon Yousaf <haroonubl0177@hotmail.com>
2023-10-21 12:44:28 +0500

    cahnegs

--- resources/views/student-dashboard.blade.php
+++ resources/views/student-dashboard.blade.php
@@ -5,11 +5,11 @@
 		</h2>
 	</x-slot>
 
-	<div class="py-12">
+	<div class="py-4 px-6 ">
 		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
 
-			<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+			<div class="p-4 lg:p-8 bg-white border-b border-gray-200">
 				Online Apply Instructions
 			</div>
 			<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex flex-wrap">
