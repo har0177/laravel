dde1e9a997fe1275f0921e6f85bf006a1be4758c
Haroon Yousaf <haroonubl0177@hotmail.com>
2025-08-25 19:16:04 +0500

    fixed

--- config/app.php
+++ config/app.php
@@ -74,7 +74,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Karachi', // or your actual timezone
 
     /*
     |--------------------------------------------------------------------------
